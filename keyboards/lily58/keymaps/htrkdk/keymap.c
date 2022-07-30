904974682f0c5f0e37ef3341fb0908aee04d433b
htrkdk <htrkdk@gmail.com>
2022-07-30 11:22:15 +0900

    FIX position of modifier keys

--- keyboards/lily58/keymaps/htrkdk/keymap.c
+++ keyboards/lily58/keymaps/htrkdk/keymap.c
@@ -9,7 +9,7 @@ enum layer_number {
 };
 
 enum custom_keycodes {
-  OUT_TOG
+  OUT_TOG = SAFE_RANGE
 };
 
 typedef union {
@@ -27,21 +27,21 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
  * | Tab  |   Q  |   W  |   E  |   R  |   T  |                    |   Y  |   U  |   I  |   O  |   P  |  -   |
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
- * |LCTRL |   A  |   S  |   D  |   F  |   G  |-------.    ,-------|   H  |   J  |   K  |   L  |   ;  |  '   |
+ * |LCTRL |   A  |   S  |   D  |   F  |   G  |-------.    ,-------|   H  |   J  |   K  |   L  |  ENT | BSPC |
  * |------+------+------+------+------+------|   [   |    |   ]   |------+------+------+------+------+------|
  * |LShift|   Z  |   X  |   C  |   V  |   B  |-------|    |-------|   N  |   M  |   ,  |   .  |   /  |RShift|
  * `-----------------------------------------/       /     \      \-----------------------------------------'
- *                   | LAlt | LGUI |LOWER | /Space  /       \Enter \  |RAISE |  BS  | DEL  |
- *                   |      |      | MHEN |/       /         \      \ | HENK |      |      |
+ *                   | LAlt | LGUI |LOWER | /Space  /       \Enter \  |RAISE | RGUI | DEL  |
+ *                   |      | MHEN |      |/       /         \      \ |      | HENK |      |
  *                   `----------------------------'           '------''--------------------'
  */
 
  [_QWERTY] = LAYOUT(
   KC_ESC,   KC_1,   KC_2,    KC_3,    KC_4,    KC_5,                     KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_GRV,
   KC_TAB,   KC_Q,   KC_W,    KC_E,    KC_R,    KC_T,                     KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_MINS,
-  KC_LCTRL, KC_A,   KC_S,    KC_D,    KC_F,    KC_G,                     KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
+  KC_LCTRL, KC_A,   KC_S,    KC_D,    KC_F,    KC_G,                     KC_H,    KC_J,    KC_K,    KC_L,    KC_ENT,  KC_BSPC,
   KC_LSFT,  KC_Z,   KC_X,    KC_C,    KC_V,    KC_B, KC_LBRC,  KC_RBRC,  KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
-                       KC_LALT, KC_LGUI, LT(_LOWER, KC_MHEN), KC_SPC, KC_ENT, LT(_RAISE, KC_HENK), KC_BSPC, KC_DEL
+           KC_LALT, LGUI_T(KC_MHEN), LT(_LOWER, KC_SPC), KC_SPC, KC_ENT, LT(_RAISE, KC_SPC), RGUI_T(KC_HENK), KC_DEL
 ),
 /* LOWER
  * ,-----------------------------------------.                    ,-----------------------------------------.
@@ -49,7 +49,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
  * | ESC  |   1  |   2  |   3  |   4  |   5  |                    |   6  |   7  |   8  |   9  |   0  |   `  |
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
- * |      |   -  |   =  |   \  |   `  |      |-------.    ,-------| HOME |PGDOWN| PGUP |  END | PSCR |      |
+ * |      |   -  |   =  |   \  |   `  |      |-------.    ,-------| HOME |PGDOWN| PGUP |  END |   ;  |  '   |
  * |------+------+------+------+------+------|   [   |    |   ]   |------+------+------+------+------+------|
  * |      |  F1  |  F2  |  F3  |  F4  |  F5  |-------|    |-------|  F6  |  F7  |  F8  |  F9  | F10  | F11  |
  * `-----------------------------------------/       /     \      \-----------------------------------------'
@@ -60,7 +60,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [_LOWER] = LAYOUT(
   _______, _______, _______, _______, _______, _______,                   _______, _______, _______, _______, _______, _______,
   KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                      KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_GRV,
-  _______, KC_MINS, KC_EQL,  KC_BSLS, KC_GRV,  XXXXXXX,                   KC_HOME, KC_PGDN, KC_PGUP, KC_END,  KC_PSCR, XXXXXXX,
+  _______, KC_MINS, KC_EQL,  KC_BSLS, KC_GRV,  XXXXXXX,                   KC_HOME, KC_PGDN, KC_PGUP, KC_END,  KC_SCLN, KC_QUOT,
   _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   _______, _______, KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,
                              _______, _______, _______, _______, _______, _______, _______, _______
 ),
