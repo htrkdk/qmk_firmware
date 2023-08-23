698e7f6e91d0eb33c4f748be3a3bd4149768a142
htrkdk <htrkdk@gmail.com>
2023-08-23 19:21:48 +0900

    CHG tapping term

--- keyboards/lily58/keymaps/htrkdk/keymap.c
+++ keyboards/lily58/keymaps/htrkdk/keymap.c
@@ -40,9 +40,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  [_QWERTY] = LAYOUT(
   KC_ESC,   KC_1,   KC_2,    KC_3,    KC_4,    KC_5,                     KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_GRV,
   KC_TAB,   KC_Q,   KC_W,    KC_E,    KC_R,    KC_T,                     KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_MINS,
-  KC_LCTRL, KC_A,   KC_S,    KC_D,    KC_F,    KC_G,                     KC_H,    KC_J,    KC_K,    KC_L,    KC_ENT,  KC_BSPC,
+  KC_LCTL,  KC_A,   KC_S,    KC_D,    KC_F,    KC_G,                     KC_H,    KC_J,    KC_K,    KC_L,    KC_ENT,  KC_BSPC,
   KC_LSFT,  KC_Z,   KC_X,    KC_C,    KC_V,    KC_B, KC_LBRC,  KC_RBRC,  KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
-           KC_LALT, LGUI_T(KC_MHEN), LT(_LOWER, KC_SPC), KC_SPC, KC_ENT, LT(_RAISE, KC_SPC), RGUI_T(KC_HENK), KC_DEL
+           KC_LALT, LGUI_T(KC_INT5), LT(_LOWER, KC_SPC), KC_SPC, KC_ENT, LT(_RAISE, KC_SPC), RGUI_T(KC_INT4), KC_DEL
 ),
 /* LOWER
  * ,-----------------------------------------.                    ,-----------------------------------------.
@@ -51,7 +51,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * | ESC  |   1  |   2  |   3  |   4  |   5  |                    |   6  |   7  |   8  |   9  |   0  |   `  |
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
  * |      |   -  |   =  |   \  |   `  |      |-------.    ,-------| HOME |PGDOWN| PGUP |  END |   ;  |  '   |
- * |------+------+------+------+------+------|   [   |    |   ]   |------+------+------+------+------+------|
+ * |------+------+------+------+------+------|   {   |    |   }   |------+------+------+------+------+------|
  * |      |      |      |      |      |      |-------|    |-------|      |      |   <  |   >  |   ?  | FUNC |
  * `-----------------------------------------/       /     \      \-----------------------------------------'
  *                   | LAlt | LGUI |LOWER | /Space  /       \Enter \  |RAISE |  BS  | DEL  |
@@ -63,7 +63,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   _______, _______, _______, _______, _______, _______,                   _______, _______, _______, _______, _______, _______,
   KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                      KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_GRV,
   _______, KC_MINS, KC_EQL,  KC_BSLS, KC_GRV,  XXXXXXX,                   KC_HOME, KC_PGDN, KC_PGUP, KC_END,  KC_SCLN, KC_QUOT,
-  _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, _______, _______, XXXXXXX, XXXXXXX, KC_LABK, KC_RABK, KC_QUES, TT(_FUNCTION),
+  _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_LCBR, KC_RCBR, XXXXXXX, XXXXXXX, KC_LABK, KC_RABK, KC_QUES, TT(_FUNCTION),
                              _______, _______, _______, _______, _______, _______, _______, _______
 ),
 
